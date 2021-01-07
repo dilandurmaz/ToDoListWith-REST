1d8f661bcb3b234e07662c93b0c262d1bd2bb88a
dilandurmaz <45289986+dilandurmaz@users.noreply.github.com>
2021-01-08 02:14:03 +0300

    methodsadded

--- app.js
+++ app.js
@@ -23,6 +23,39 @@ app.get('/getTodos',(req,res)=>{
 });
 
 
+app.put('/:id',(req,res)=>{
+    const todoID = req.params.id;
+    const userInput = req.body;
+
+    db.getDB().collection(collection).findOneAndUpdate({_id:db.getPrimaryKey(todoID)},{$set:{todo:userInput.todo}},{returnOriginal:false},(err,result)=>{
+        if(err)
+            console.log(err);
+        else
+            res.json(result);
+    })
+
+} )
+
+app.post('/', (req,res)=>{
+    const userInput = req.body;
+    db.getDB().collection(collection).insertOne(userInput,(err,result)=>{
+        if(err)
+            console.log(err);
+        else
+            res.json({result:result,document:result.ops[0]});
+    })
+})
+
+app.delete('/:id',(req,res)=>{
+    const todoID = req.params.id;
+
+    db.getDB().collection(collection).findOneAndDelete({_id : db.getPrimaryKey(todoID)},(err,result)=>{
+        if(err)
+            console.log(err);
+        else
+            res.json(result);
+    })
+})
 db.connect((err)=>{
     if(err){
         console.log('Unable to connect to database');
